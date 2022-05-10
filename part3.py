0f48e8d42d73143d38c70bfafb66b1a32c1483d8
Stephen Jones <103528513+stephenj0nes@users.noreply.github.com>
2022-05-10 00:32:57 +0000

    finished

--- part3.py
+++ part3.py
@@ -19,4 +19,16 @@ positive
 not divisible by 3
 '''
 
-#write your code below
+number = int(input("Enter a number: "))
+
+if number > 0:
+  print("positive")
+elif number < 0:
+  print("negative")
+else:
+  print("zero")
+  
+if (number % 3) == 0:
+  print("divisible by 3")
+else:
+  print("not divisible by 3")
--- part4.py
+++ part4.py
@@ -23,4 +23,14 @@ Enter another number:  -2
 opposite
 '''
 
-#start writing your code below
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+
+if number1 > 0 and number2 > 0:
+  print("positive")
+elif number1 < 0 and number2 < 0:
+  print("negative")
+elif number1 == 0 or number2 == 0:
+  print("zero")
+elif (number1<0 and number2>0) or (number1>0 and number2<0):
+  print("opposite")
